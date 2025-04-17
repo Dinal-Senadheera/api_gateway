cff7b2fabd314aa465eb852e8df84a60db68fc9a
Dinal-Senadheera <dinalsenadheera@gmail.com>
2025-04-17 14:37:57 +0530

    minor bug ifx

--- src/authentication.middleware.ts
+++ src/authentication.middleware.ts
@@ -7,7 +7,7 @@ export class AuthenticationMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     // Public routes that don't require authentication
     const publicRoutes = ['/api/auth/google', '/api/auth/google/callback'];
-    if (publicRoutes.some((route) => req.path.startsWith(route))) {
+    if (publicRoutes.some((route) => req.originalUrl.includes(route))) {
       return next();
     }
 
