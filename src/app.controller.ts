871af2adf4fd7b929ece7ea8e789ca189bd52226
Dinal-Senadheera <dinalsenadheera@gmail.com>
2025-04-17 23:30:56 +0530

    remove unwanted code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,8 +16,7 @@ export class AppController {
   async getAuth(@Req() req, @Res() res) {
     try {
       console.log('redirecting to auth service', req.originalUrl);
-      const urlPath = req.originalUrl.replace('/api/auth', '/api/auth');
-      console.log(urlPath);
+      const urlPath = req.originalUrl;
 
       const response = await axios.get(`${this.AUTH_ENDPOINT}${urlPath}`, {
         headers: {
