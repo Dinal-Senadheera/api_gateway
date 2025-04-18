294d8877c6692eef48f62312478bbf068840ef02
Dinal-Senadheera <dinalsenadheera@gmail.com>
2025-04-18 05:46:34 +0530

    support cookies

--- src/authentication.middleware.ts
+++ src/authentication.middleware.ts
@@ -12,17 +12,24 @@ export class AuthenticationMiddleware implements NestMiddleware {
       return next();
     }
 
-    // Check for token in Authorization header
+    let token: string | undefined = undefined;
+
+    // First check Authorization header
     const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      // No token = not logged in
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      token = authHeader.split(' ')[1];
+    }
+    // If no Authorization header, check for cookie
+    else if (req.cookies && req.cookies.auth_token) {
+      token = req.cookies.auth_token;
+    }
+
+    if (!token) {
       return res.status(401).json({
         success: false,
         message: `Authentication required`,
       });
     }
-
-    const token = authHeader.split(' ')[1];
 
     try {
       // Verify token manually using jsonwebtoken package
